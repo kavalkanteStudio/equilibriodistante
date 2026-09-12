51c087c40e62497bedcaca59511447b51638a4da
kavalkanteStudio <kavalkante@proton.me>
2026-09-12 16:01:03 -0400

    claude code - phase 3 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CollectionDetail from './pages/CollectionDetail'
+import ArtworkDetail from './pages/ArtworkDetail'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection/:slug" element={<CollectionDetail />} />
+        <Route path="/artwork/:slug" element={<ArtworkDetail />} />
         {/* Other routes will be added here as we implement them */}
       </Routes>
     </main>
